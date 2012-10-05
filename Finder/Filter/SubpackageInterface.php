b57d7b3cd22d4a68f7a0a9f8a3478fde13e3b621
ackermannd <ackermann.d@gmail.com>
2012-10-06 00:04:32 +0200

    added missing license information

--- Finder/Filter/SubpackageInterface.php
+++ Finder/Filter/SubpackageInterface.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * (c) Daniel Ackermann
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace ackermannd\File\Finder\Filter;
 
 interface SubpackageInterface {
